af40aa981cfb47db8f14d28adb43ab55b59146aa
Cristian Carpenter <ccarpenterg@gmail.com>
2013-12-19 12:05:42 -0300

    Added intercept_name parameter to trainingExamples.

--- logistic_regression/base.py
+++ logistic_regression/base.py
@@ -4,11 +4,11 @@ import numpy as np
 def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
 
-def trainingExamples(path_to_csv, training_examples):
+def trainingExamples(path_to_csv, training_examples, intercept_name):
     df = pd.read_csv(path_to_csv)
     m = len(df.index)
-    df2 = pd.DataFrame(np.ones(m), columns=('pixel',))
-    df.insert(1, 'pixel', df2['pixel'])
+    df2 = pd.DataFrame(np.ones(m), columns=(intercept_name,))
+    df.insert(1, intercept_name, df2[intercept_name])
     rows = df1.index
     np.random.shuffle(list(rows))
     df.reindex(rows)
