489a6d8cb5c315857fa0d9993ef451d16aa7c340
Cristian Carpenter <ccarpenterg@gmail.com>
2013-12-19 17:21:11 -0300

    Changed trainingExamples function to return entire dataframe, X, y, Xval and yval.

--- logistic_regression/base.py
+++ logistic_regression/base.py
@@ -4,13 +4,17 @@ import numpy as np
 def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
 
-def trainingExamples(path_to_csv, training_examples, intercept_name):
+def trainingExamples(path_to_csv, training_examples, label, intercept_name):
     df = pd.read_csv(path_to_csv)
     m = len(df.index)
     df2 = pd.DataFrame(np.ones(m), columns=(intercept_name,))
     df.insert(1, intercept_name, df2[intercept_name])
-    rows = df1.index
+    features = list(df.columns)
+    features.remove(label)
+    rows = df.index
     np.random.shuffle(list(rows))
     df.reindex(rows)
-    return df, df[:training_examples], df[training_examples:]
+    X, y = df[:training_examples].ix[:, features[0]:features[-1]], df[:training_examples][label]
+    Xval, yval = df[training_examples:].ix[:, features[0]:features[-1]], df[training_examples:][label]
+    return df, X, y, Xval, yval
 
