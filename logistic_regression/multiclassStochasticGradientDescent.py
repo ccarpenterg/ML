ada05ea010123d55546c80eb28491c3423ca3f1f
Cristian Carpenter <ccarpenterg@gmail.com>
2013-12-27 19:28:35 -0300

    Fixed multiclassStochasticGradientDescent, prediction, accuracy.

--- logistic_regression/multiclassStochasticGradientDescent.py
+++ logistic_regression/multiclassStochasticGradientDescent.py
@@ -1,15 +1,25 @@
+import numpy as np
+from base import sigmoid
+from stochasticGradientDescent import stochasticGradientDescent
 
-def multiclassStochasticGradientDescent(X, classes, labels, iterations, alpha, Lambda, oscillator_factor=4):
+def multiclassStochasticGradientDescent(X, labels, classes, iterations, alpha, Lambda, oscillator_factor=4):
     k = len(classes)
     m, n = X.shape
     all_thetas = np.zeros(shape=(k, n))
     for c in classes:
-        y = (labels == c)
+        print 'Running SGD for label', c
+        y = (labels == c).map(int)
         theta, costList = stochasticGradientDescent(X, y, iterations, alpha, Lambda, oscillator_factor)
         all_thetas[c] = theta
     return all_thetas
 
 def prediction(X, all_thetas):
     X = np.matrix(X)
-    prediction = np.argmax(sigmoid(np.dot(X, all_thetas)), axis=1)
+    prediction = np.argmax(sigmoid(np.dot(X, all_thetas.transpose())), axis=1)
+    prediction = np.squeeze(np.asarray(prediction))
     return prediction
+
+def accuracy(y, prediction):
+    accur = (y == prediction)
+    accur = accur.astype(int)
+    return (float(accur.sum()) / accur.size) * 100
