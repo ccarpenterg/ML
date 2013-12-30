c43184ab8a2f556346ec8027f431c2cf5bda8d32
Cristian Carpenter <ccarpenterg@gmail.com>
2013-12-30 15:34:13 -0300

    Added prediction for Newton CG. Fixed cost and gradient. Added scipy import.

--- logistic_regression/newtonConjugateGradient.py
+++ logistic_regression/newtonConjugateGradient.py
@@ -1,20 +1,26 @@
 import numpy as np
+from scipy import optimize
 from base import sigmoid
 
 def cost(theta, X, y, m):
     H = sigmoid(np.dot(X, theta))
-    return (-1/m) * (np.dot(y.transpose(), np.log(H)) + np.dot((1-y).transpose(), np.log(1-H)))
+    return (-1./m) * (np.dot(y.transpose(), np.log(H)) + np.dot((1-y).transpose(), np.log(1-H)))
 
 def gradient(theta, X, y, m):
     H = sigmoid(np.dot(X, theta))
-    return (1/m) * np.dot(X.transpose(), H - y)
+    return (1./m) * np.dot(X.transpose(), H - y)
 
-def newtonConjugateGradient(X, labels, classes, opt):
+def newtonConjugateGradient(X, labels, classes):
     k = len(classes)
     m, n = X.shape
     all_thetas = np.zeros(shape=(k, n))
     for c in classes:
+        print 'Running SGD for label', c
         y = (labels == c).astype(int)
         theta0 = np.zeros(n)
-        theta = scipy.optimize.fmin_cg(cost, theta0, fprime=gradient, args=(X, y, m))
+        theta = optimize.fmin_cg(cost, theta0, fprime=gradient, args=(X, y, m))
         all_thetas[c] = theta
+    return all_thetas
+
+def prediction(X, all_thetas):
+    return np.argmax(sigmoid(np.dot(X, all_thetas.transpose())), axis=1)
