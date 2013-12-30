eac2a5e913cf17b632985ae11f327f0f1cb09ebd
Cristian Carpenter <ccarpenterg@gmail.com>
2013-12-29 21:28:04 -0300

    Added multiclassification algorithm using Newton Conjugate Gradient (scipy).

--- logistic_regression/newtonConjugateGradient.py
+++ logistic_regression/newtonConjugateGradient.py
@@ -8,3 +8,13 @@ def cost(theta, X, y, m):
 def gradient(theta, X, y, m):
     H = sigmoid(np.dot(X, theta))
     return (1/m) * np.dot(X.transpose(), H - y)
+
+def newtonConjugateGradient(X, labels, classes, opt):
+    k = len(classes)
+    m, n = X.shape
+    all_thetas = np.zeros(shape=(k, n))
+    for c in classes:
+        y = (labels == c).astype(int)
+        theta0 = np.zeros(n)
+        theta = scipy.optimize.fmin_cg(cost, theta0, fprime=gradient, args=(X, y, m))
+        all_thetas[c] = theta
